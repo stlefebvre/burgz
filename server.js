83fa832bc0916d7ac2059c798e6d69d04b46a1df
stlefebvre <lefebvre.shelby@gmail.com>
2018-10-09 19:41:45 -0400

    connection

--- server.js
+++ server.js
@@ -18,4 +18,6 @@ app.set("view engine", "handlebars");
 
 app.use("/", routes);
 
-app.listen(PORT);
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
